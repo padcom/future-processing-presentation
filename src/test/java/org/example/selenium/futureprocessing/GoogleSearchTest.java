e1ef7632ae0a3e3f7ee39c43b7921987e7006bcd
Maciej Hryniszak <maciej.hryniszak@lumesse.com>
2015-04-28 23:05:58 +0200

    Added test that uses manual steps to show how it's done inside a test

--- src/test/java/org/example/selenium/futureprocessing/GoogleSearchTest.java
+++ src/test/java/org/example/selenium/futureprocessing/GoogleSearchTest.java
@@ -6,7 +6,15 @@ import org.junit.Test;
 
 public class GoogleSearchTest extends SeleniumTest {
 	@Test
-	public void search_for_future_processing() {
+	public void search_for_future_processing_1() {
+		new GoogleHomePage(driver)
+			.search("future processing")
+			.selectNthResult(0)
+			.assertPageHeaderExists();
+	}
+
+	@Test
+	public void search_for_future_processing_2() {
 		new GoogleHomePage(driver)
 			.run(new EnterFutureProcessingPageViaGoogleSearchScenario())
 			.assertPageHeaderExists();
